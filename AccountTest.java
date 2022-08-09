7341028e1a9a22bc55c3ec2022ba2e6c34da80e9
Harish mahur <hmahur20@gmail.com>
2022-08-09 08:18:40 +0530

    modified:stock management

---
+++ AccountTest.java
@@ -0,0 +1,10 @@
+package com.oopsconcept;
+
+public class AccountTest {
+	public static void main(String[] args) {
+		Account account = new Account();
+		StockPortfolio stockPortfolio = new StockPortfolio();
+		account.debit(stockPortfolio.totalValue);
+	}
+
+}
